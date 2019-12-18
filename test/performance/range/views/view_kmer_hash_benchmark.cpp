2cfd2a142c7c7ada4ad0c087ad074dbb11419a65
ylapawczyk <ylapawczyk@zedat.fu-berlin.de>
2019-12-18 21:35:27 +0100

    added view for kmer hashing with nthash

--- test/performance/range/views/view_kmer_hash_benchmark.cpp
+++ test/performance/range/views/view_kmer_hash_benchmark.cpp
@@ -9,6 +9,7 @@
 
 #include <seqan3/alphabet/nucleotide/dna4.hpp>
 #include <seqan3/range/views/kmer_hash.hpp>
+#include <seqan3/range/views/kmer_nthash.hpp>
 #include <seqan3/test/performance/sequence_generator.hpp>
 #include <seqan3/test/performance/naive_kmer_hash.hpp>
 #include <seqan3/test/performance/units.hpp>
@@ -63,6 +64,25 @@ static void seqan_kmer_hash_ungapped(benchmark::State & state)
     for (auto _ : state)
     {
         for (auto h : seq | views::kmer_hash(ungapped{static_cast<uint8_t>(k)}))
+            benchmark::DoNotOptimize(sum += h);
+    }
+
+    state.counters["Throughput[bp/s]"] = bp_per_second(sequence_length - k + 1);
+}
+
+static void seqan_kmer_nthash_ungapped(benchmark::State & state)
+{
+    auto sequence_length = state.range(0);
+    assert(sequence_length > 0);
+    size_t k = static_cast<size_t>(state.range(1));
+    assert(k > 0);
+    auto seq = test::generate_sequence<dna4>(sequence_length, 0, 0);
+
+    volatile size_t sum{0};
+
+    for (auto _ : state)
+    {
+        for (auto h : seq | views::kmer_nthash(ungapped{static_cast<uint8_t>(k)}))
             benchmark::DoNotOptimize(sum += h);
     }
 
@@ -175,6 +195,7 @@ BENCHMARK(seqan2_kmer_hash_gapped)->Apply(arguments);
 #endif // SEQAN3_HAS_SEQAN2
 
 BENCHMARK(seqan_kmer_hash_ungapped)->Apply(arguments);
+BENCHMARK(seqan_kmer_nthash_ungapped)->Apply(arguments);
 BENCHMARK(seqan_kmer_hash_gapped)->Apply(arguments);
 BENCHMARK(naive_kmer_hash)->Apply(arguments);
 
